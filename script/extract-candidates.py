14798b993c6e18d97afc3400a9a04a8db2dc0160
hsh814 <hsh814@naver.com>
2023-01-12 23:41:07 -0800

    Update extract-candidates.py

--- script/extract-candidates.py
+++ script/extract-candidates.py
@@ -12,6 +12,40 @@ def get_plausible(sim: List[dict]) -> List[str]:
       plau_list.append(loc)
   return plau_list
 
+def get_info_tbar(sw: dict, correct_patches: List[str], plau_list: List[str]) -> Dict[str, dict]:
+  import shutil
+  for project in os.listdir("tbar_plausible/"):
+      project_name = project.split("-")[0]
+      candidates = json.load(open(f"tbar_plausible/{project}"))
+      
+      if project_name not in os.listdir(f"/root/project/TBarCopy/results/"):
+          continue
+      
+      if not os.path.exists(f"/root/project/TBarCopy/script/plausible_patches/{project_name}"):
+          os.makedirs(f"/root/project/TBarCopy/script/plausible_patches/{project_name}")
+      
+      for candidate in candidates:
+          path = next(iter(candidate.keys()))
+          path = path.split("/")[1]
+          
+          for patch in os.listdir(f"/root/project/TBarCopy/results/{project_name}"):
+              if patch != path:
+                  continue
+              shutil.copyfile(f"/root/project/TBarCopy/results/{project_name}/{patch}", 
+                        f"/root/project/TBarCopy/script/plausible_patches/{project_name}/{patch}")
+
+  for project in os.listdir("plausible_patches/"):
+      for idx in os.listdir(f"plausible_patches/{project}"):
+          diff_folder = project + "-" + idx[:idx.rfind("_")].replace("_", "-")
+          
+          file = os.listdir(f"plausible_patches/{project}/{idx}")[0]
+          modif_file = os.listdir(f"plausible_patches/{project}/{idx}")[0].split(".")[0]
+          
+          if not os.path.exists(f"coming_rep/{diff_folder}/{modif_file}"):
+              os.makedirs(f"coming_rep/{diff_folder}/{modif_file}")
+              
+          shutil.copyfile(f"plausible_patches/{project}/{idx}/{file}", 
+                          f"coming_rep/{diff_folder}/{modif_file}/{diff_folder}_{modif_file}_t.java")
 
 def get_info_recoder(sw: dict, correct_patches: List[str], plau_list: List[str]) -> Dict[str, dict]:
   result = dict()
@@ -49,7 +83,34 @@ def get_correct_patch_recoder(correct_patch_file: str) -> List[str]:
       result[bid] = correct_patches
     return result
 
-
+def collect_plausible_recoder(rootdir: str, outdir: str) -> None:
+  for bugid in os.listdir(os.path.join(rootdir, "d4j")):
+    os.makedirs(f"{outdir}/{bugid}", exist_ok=True)
+    switch_info_file = os.path.join(rootdir, "d4j", bugid, "switch-info.json")
+    sim_file = os.path.join(rootdir, "sim", bugid, f"{bugid}-sim.json")
+    with open(switch_info_file, "r") as swf, open(sim_file, "r") as sf:
+      sw = json.load(swf)
+      sim = json.load(sf)
+      plau_list = get_plausible(sim)
+      result = dict()
+      result["bugid"] = sw["project_name"]
+      result["tool"] = "recoder"
+      result["correct_patch"] = dict()
+      plau_patches = list()
+      result["plausible_patches"] = plau_patches
+      for file_info in sw["rules"]:
+        file_name = file_info["file"]
+        for line_info in file_info["lines"]:
+          for case_info in line_info["cases"]:
+            loc = case_info["location"]
+            tokens = loc.split("/")
+            id = tokens[0] + "-" + tokens[1]
+            patch_info = {"id": id, "location": loc, "file": file_name, "line": line_info["line"]}
+            if loc in plau_list:
+              plau_patches.append(patch_info)
+      with open(f"{outdir}/{bugid}/{bugid}.json", "w") as f:
+        json.dump(result, f, indent=2)
+      
 def main_recoder(args: List[str]) -> None:
   rootdir = args[1]
   outdir = args[2]
@@ -65,13 +126,52 @@ def main_recoder(args: List[str]) -> None:
       sim = json.load(sf)
       plau_list = get_plausible(sim)
       result = get_info_recoder(sw, correct_patches, plau_list)
-      with open(f"{outdir}/{bugid}.json", "w") as f:
+      with open(f"{outdir}/{bugid}/{bugid}.json", "w") as f:
         json.dump(result, f, indent=2)
     for plau in plau_list:
       original = os.path.join(rootdir, "d4j", bugid, plau)
       target = os.path.join(outdir, bugid, plau)
       os.makedirs(os.path.dirname(target), exist_ok=True)
       os.system(f"cp {original} {target}")
+
+def main_tbar(args):
+  rootdir = args[1]
+  outdir = args[2]
+  for bugid in os.listdir(os.path.join(rootdir, "d4j")):
+    bugid_recoder = bugid.replace("_", "-")
+    os.makedirs(f"{outdir}/{bugid_recoder}", exist_ok=True)
+    switch_info_file = os.path.join(rootdir, "d4j", bugid, "switch-info.json")
+    sim_file = os.path.join(rootdir, "sim", bugid, f"{bugid}-sim.json")
+    with open(switch_info_file, "r") as swf, open(sim_file, "r") as sf:
+      sw = json.load(swf)
+      sim = json.load(sf)
+      plau_list = get_plausible(sim)
+      result = dict()
+      result["bugid"] = bugid_recoder
+      result["tool"] = "tbar"
+      result["correct_patch"] = dict()
+      plau_patches = list()
+      result["plausible_patches"] = plau_patches
+      for file_info in sw["rules"]:
+        file_name = file_info["file"]
+        for line_info in file_info["lines"]:
+          for case_info in line_info["switches"]:
+            loc = case_info["location"]
+            tokens = loc.split("/")
+            id = loc.replace(tokens[-1], "")
+            id = id.replace("/", "_")
+            patch_info = {"id": id, "location": loc, "file": file_name, "line": line_info["line"]}
+            if loc in plau_list:
+              plau_patches.append(patch_info)
+      with open(f"{outdir}/{bugid_recoder}/{bugid_recoder}.json", "w") as f:
+        json.dump(result, f, indent=2)
+      for plau in plau_list:
+        original = os.path.join(rootdir, "d4j", bugid, plau)
+        target = os.path.join(outdir, bugid_recoder, plau)
+        os.makedirs(os.path.dirname(target), exist_ok=True)
+        os.system(f"cp {original} {target}")
+
+
 
 if __name__ == "__main__":
   main_recoder(sys.argv)
